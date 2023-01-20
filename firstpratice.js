c1f7cf48ff091cb298d8bf7d47b85e3d05677ae3
GAFmedia <110905154+GAFmedia@users.noreply.github.com>
2023-01-20 18:18:25 +0100

    Add files via upload

---
+++ firstpratice.js
@@ -0,0 +1,9 @@
+console.log ("Hello World!");
+console.log ("hello" + " " +"world" + "!" .toUpperCase);
+console.log (typeof 10);
+console.log (parseInt ("13790" + 50));
+console.log ("Afolake you are beautiful" .charAt(8));
+console.log ("WElcome to shege banza" .toLocaleUpperCase);
+var ContentFilled = (true);
+console.log (ContentFilled === true);
+
