863e1b6df03f0f2c9aa4a4638fc85577ecea8cf2
GAFmedia <110905154+GAFmedia@users.noreply.github.com>
2023-01-20 18:19:54 +0100

    Update firstpratice.js

--- firstpratice.js
+++ firstpratice.js
@@ -7,3 +7,51 @@ console.log ("WElcome to shege banza" .toLocaleUpperCase);
 var ContentFilled = (true);
 console.log (ContentFilled === true);
 
+console.log ("Hello World!");
+console.log ("hello" + " " +"world" + "!" .toUpperCase);
+console.log (typeof 10);
+console.log (parseInt ("13790" + 50));
+console.log ("Afolake you are beautiful" .charAt(8));
+console.log ("WElcome to shege banza" .toLocaleUpperCase);
+var ContentFilled = (true);
+console.log (ContentFilled === true);
+var Upwork;
+console.log (Boolean (Upwork));
+if (Upwork===true) {
+    console.log (Lovely);
+} else {
+    console.log (update);
+}
+var List ={
+    1 : Beans,
+    2 : Potatoes,
+    3 : Garri,
+    4 : Kulikuli,
+    5 : Leftover,
+};
+console.log (list);
+var office = {
+    CompanyName : GAFMEDIA,
+    Position : Chiefexecutiveofficer,
+    FirstName : Afolake,
+    LastName : Adefioye,
+    MiddleName : Grace,
+    number : {
+        PhoneNumber : "07017304358",
+        Address : "125,roof street",
+    },
+    Age : 22,
+    Sex : Female,
+    Height : 5.5,
+};
+    console.log (office.Firstname);
+    console.log (office.number.PhoneNumber);
+    var Salad = {
+        carrot : available,
+        cream : available,
+        cucumber : "not available",
+        Apple : "2 slice",
+        sayveggies : function veggies(params) {
+            console.log (this.cucumber + "allowed");
+        }
+    };
